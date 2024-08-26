84a7491be19c8ac6d562e3c2939b1e4742463291
deathsolitary <deadlydarknosferatu@yandex.ru>
2024-08-26 09:35:17 +0300

    Add files via upload

---
+++ docker-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt install curl software-properties-common ca-certificates apt-transport-https -y
+wget -O- https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo tee /etc/apt/keyrings/docker.gpg > /dev/null
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable"| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce -y
+systemctl status docker
